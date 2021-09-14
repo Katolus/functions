50e4034ccad37d1773919f69c6ce195a2deeb642
Katolus <pkatolik@healthshare.com.au>
2021-09-14 14:39:47 +1000

    Add validators and errors

--- functions/errors.py
+++ functions/errors.py
@@ -1,3 +1,14 @@
+from pydantic import PydanticValueError
+
+
 class FunctionsError(Exception):
     ...
 
+
+
+class ItIsNotAValidDirectory(PydanticValueError, FunctionsError):
+    msg_template = "value is not a valid function directory"
+
+
+class ConfigNotFoundError(PydanticValueError, FunctionsError):
+    msg_template = "value does not include a valid config file"
---
+++ functions/validators.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+from typing import Any
+
+from pydantic.errors import PathNotADirectoryError
+
+from functions.errors import ConfigNotFoundError
+
+
+def path_dir_validator(v: Path) -> Path:
+    if not v.is_dir():
+        raise PathNotADirectoryError(path=v)
+
+    return v
+
+
+def path_has_config_validator(v: Any) -> Path:
+    if not Path(os.path.join(v, "config.json")).exists():
+        raise ConfigNotFoundError(path=v)
+
+    return v
