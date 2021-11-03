739ed5073146b41bd18530510a46f0f9d891f2c8
Katolus <katolus.work@gmail.com>
2021-11-03 19:12:33 +1100

    An effort to simplify errors

--- functions/errors.py
+++ functions/errors.py
@@ -1,63 +1,46 @@
 from typing import Any
 
-from pydantic import PydanticValueError
-
-from functions.mixins import FunctionErrorMixin
-from functions.types import CallableGenerator
-from functions.types import PathStr
+from functions.types import ExceptionClass
 
 
-class FunctionBaseError(FunctionErrorMixin, Exception):
-    # Pydantic
-    @classmethod
-    def __get_validators__(cls) -> CallableGenerator:
-        yield cls.validate
+class FunctionBaseError(Exception):
+    code: str
+    msg_template: str
+    original_error: ExceptionClass
 
-    @classmethod
-    def validate(cls, value: Any) -> Any:
-        # if not issubclass(value, cls):
-        #     raise ConfigError(f"'{value}'Is not a valid exception class")
-        return value
+    def __init__(self, *, error: ExceptionClass, **kwargs: Any) -> None:
+        self.original_error = error
+        self.__dict__ = kwargs
+        super().__init__()
 
+    def __str__(self) -> str:
+        return self.msg_template.format(**self.__dict__)
 
-class _DerivedError(FunctionBaseError):
-    def __init__(self, *, error: Exception) -> None:
-        super().__init__(error=error)
-
-
-class _PathValueError(PydanticValueError):
-    def __init__(self, *, path: PathStr) -> None:
-        super().__init__(path=str(path))
+    @property
+    def message(self) -> str:
+        return str(self)
 
 
-class IsNotAValidDirectory(_PathValueError, FunctionBaseError):
+class IsNotAValidDirectory(FunctionBaseError):
     code = "path.invalid_directory"
     msg_template = "Path '{path}' is not a valid function directory"
 
 
-class ConfigNotFoundError(_PathValueError, FunctionBaseError):
+class ConfigNotFoundError(FunctionBaseError):
     code = "path.config_not_found"
     msg_template = "Path '{path}' does not include a valid config file"
 
 
-class PathNotADirectoryError(_PathValueError):
+class PathNotADirectoryError(FunctionBaseError):
     code = "path.not_a_directory"
     msg_template = 'Path "{path}" does not point to a directory'
 
 
-class FunctionBuildError(_DerivedError):
+class FunctionBuildError(FunctionBaseError):
     code = "build.error"
     msg_template = "Function build has field with the following error -> {error}"
-
-
-class ConfigValidationError(_DerivedError):
-    code = "config.validation"
-    msg_template = "Config file validation failed with error -> {error}"
 
 
 class FunctionNameTaken(FunctionBaseError):
     code = "functions.name_taken"
     msg_template = "Function with that name ('{name}') already exists"
-
-    def __init__(self, name: str, /) -> None:
-        super().__init__(name=name)
