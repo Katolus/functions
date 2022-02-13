c563224039ae2707f18cbc5181ebe48aa7f52810
Katolus <katolus.work@gmail.com>
2022-02-13 18:14:11 +1100

    Update docker labels to represent new values

--- functions/docker/enums.py
+++ functions/docker/enums.py
@@ -6,8 +6,11 @@ from enum import unique
 class DockerLabel(str, Enum):
     """Stores constants under which variables are stored"""
 
-    CONFIG = "package.functions.config"
-    CONFIG_PATH = "package.functions.config_path"
-    FUNCTION_NAME = "package.functions.function_name"
-    FUNCTION_PATH = "package.functions.function_path"
-    ORGANISATION = "package.functions.organisation"
+    CONFIG_CONTENT = "org.ventress.functions.image.config.content"
+    CONFIG_PATH = "org.ventress.functions.image.config.path"
+    DESCRIPTION = "org.ventress.functions.image.description"
+    FUNCTION_NAME = "org.ventress.functions.image.name"
+    FUNCTION_SOURCE = "org.ventress.functions.image.source"
+    MARK = "org.ventress.functions.image.mark"
+    VENDOR = "org.ventress.functions.image.vendor"
+    VERSION = "org.ventress.functions.image.version"
